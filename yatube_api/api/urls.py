1866350ca9fad646b310966184381b7f5599fc66
Денис Шведуненко <shvedunenkodenis@gmail.com>
2025-02-27 21:16:04 +0300

    add api for comments

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,17 +1,16 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken import views
-from .views import PostViewSet, GroupViewSet
+from .views import PostViewSet, GroupViewSet, CommentViewSet
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'groups', GroupViewSet)
+router_v1 = routers.DefaultRouter()
+router_v1.register(r'posts', PostViewSet, basename='post')
+router_v1.register(r'groups', GroupViewSet, basename='group')
+router_v1.register(r'posts/(?P<post_id>[\d]+)/comments',
+                   CommentViewSet, basename='comment')
 
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
-    path('', include(router.urls)),
+    path('', include(router_v1.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
 from rest_framework import viewsets
-
-from posts.models import Post, Group
-from .serializers import PostSelializer, GroupSelializer
+from django.shortcuts import get_object_or_404
+from posts.models import Post, Group, Comment
+from .serializers import PostSelializer, GroupSelializer, CommentSelializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    
+
     queryset = Post.objects.all()
     serializer_class = PostSelializer
 
@@ -29,3 +28,30 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Group.objects.all()
     serializer_class = GroupSelializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSelializer
+
+    def get_post(self):
+        post_id = self.kwargs.get("post_id")
+        post = get_object_or_404(Post, id=post_id)
+        return post
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user,
+                        post=self.get_post())
+
+    def get_queryset(self):
+        new_queryset = Comment.objects.filter(post=self.get_post())
+        return new_queryset
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(CommentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(CommentViewSet, self).perform_destroy(instance)
